4fe7581e9ff299d2a40e93e43e575dc7046c5217
YuYang <yyangplus@gmail.com>
2017-06-27 15:10:55 +0800

    better container_of

--- src/defines.h
+++ src/defines.h
@@ -29,7 +29,8 @@
  * @member:	the name of the member within the struct.
  *
  */
-#define container_of(ptr, type, member) \
-    ((type *)(((char *)ptr) - offsetof(type,member)))
+#define container_of(ptr, type, member) ({ \
+     const typeof( ((type *)0)->member ) *__mptr = (ptr); \
+     (type *)( (char *)__mptr - offsetof(type,member) );})
 
 #endif /* _DEFINES_H_ */
